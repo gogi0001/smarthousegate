5aa71aebe57dfe2e3839583f71845a5713b03f5e
gogi0001 <38492541+gogi0001@users.noreply.github.com>
2018-11-28 14:56:54 +0300

    Add GA response

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.send('Hello there dude!'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => {
+      console.log('Somebody requested root!');
+      res.send('Hello there dude!');
+  })
+  .get('/action', (req, res) => {
+    console.log('Ouch! Is seems Google Actions ACTS!');
+    res.send('OK');
+})
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
